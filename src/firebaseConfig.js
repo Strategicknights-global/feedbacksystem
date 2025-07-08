e21594d0d8d20b3a1a2eb7f05aabd8ae77b45b53
Arshad-12 <arshadahmed1908@gmail.com>
2025-07-08 15:26:19 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,17 +3,18 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC_LlG7A66VusG9Lt01kvTnjCTOTUrl1eg",
+  authDomain: "feedback-14f0d.firebaseapp.com",
+  projectId: "feedback-14f0d",
+  storageBucket: "feedback-14f0d.firebasestorage.app",
+  messagingSenderId: "580893389642",
+  appId: "1:580893389642:web:bdb7c73e8e1cde932e87b3",
+  measurementId: "G-LJ8SMN1J12"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Export Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
